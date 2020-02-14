b368857f232340dcfe8f590ca8092473f9bdd822
hexter2018 <phongthep.kd@gmail.com>
2020-02-14 17:16:17 +0700

    f

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BaaclifeAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
